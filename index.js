6d909b11fd49ea10236505c697cb468aef3c3198
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-09-13 17:14:51 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const PORT = process.env.MY_PORT
+const apiRoutes = require('./router')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+app.use('/', apiRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`)
+})
